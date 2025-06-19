7c4e4ebb6d00f7f97961e7cf1f951f09ac020b53
Ratih Paramita <significantratih@gmail.com>
2025-06-19 17:47:53 +0700

    Mengubah Teks Placeholder Search Bar

--- fronend/src/layouts/MainLayout.js
+++ fronend/src/layouts/MainLayout.js
@@ -131,7 +131,7 @@ const Sidebar = ({ isOpen }) => {
                     <div className="relative">
                         <input
                             type="text"
-                            placeholder="Searching..."
+                            placeholder="Search"
                             className="w-full pl-4 pr-10 py-2 rounded-lg bg-gray-700 text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                             value={searchTerm}
                             onChange={handleSearchChange}
